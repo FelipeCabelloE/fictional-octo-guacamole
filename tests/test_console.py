7926509358171fd134d791d0adba5fc50c9f0833
FelipeCabelloE <98831429+FelipeCabelloE@users.noreply.github.com>
2023-09-08 13:35:30 -0300

    Added some mock test that im not particulary happy

--- tests/test_console.py
+++ tests/test_console.py
@@ -11,7 +11,28 @@ from fictional_octo_guacamole import console
 def runner():
     return click.testing.CliRunner()
 
+@pytest.fixture
+def mock_requests_get(mocker):
+    mock = mocker.patch("requests.get")
+    mock.return_value.__enter__.return_value.json.return_value = {
+        "title": "Lorem Ipsum",
+        "extract": "Lorem ipsum dolor sit amet",
+    }
+    return mock
 
-def test_main_succeeds(runner):
+def test_main_succeeds(runner, mock_requests_get):
     result = runner.invoke(console.main)
     assert result.exit_code == 0
+    
+def test_main_prints_title(runner, mock_requests_get):
+    result = runner.invoke(console.main)
+    assert "Lorem Ipsum" in result.output
+    
+def test_main_invokes_requests_get(runner, mock_requests_get):
+    runner.invoke(console.main)
+    assert mock_requests_get.called
+    
+def test_main_uses_en_wikipedia_org(runner, mock_requests_get):
+    runner.invoke(console.main)
+    args, _ = mock_requests_get.call_args
+    assert "en.wikipedia.org" in args[0]
